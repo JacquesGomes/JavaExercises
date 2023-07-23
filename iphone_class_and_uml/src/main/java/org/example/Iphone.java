6e737cc6f80310066010d1ea6a9b080b2e6e06b5
Jacques Gomes <113613459+JacquesGomes@users.noreply.github.com>
2023-07-23 10:29:22 -0300

    ok

--- iphone_class_and_uml/src/main/java/org/example/Iphone.java
+++ iphone_class_and_uml/src/main/java/org/example/Iphone.java
@@ -15,47 +15,76 @@ public class Iphone implements IMusicPlayer, IPhoneDevice, IInternetBrowser{
     //Music Player methods
     @Override
     public void play() {
-
+        if(!isPlaying){
+            System.out.println("Music playing.");
+            isPlaying = true;
+        } else{
+            System.out.println("Music is already playing.");
+        }
     }
 
     @Override
     public void pause() {
-
+        if(isPlaying){
+            System.out.println("Music paused.");
+            isPlaying = false;
+        } else{
+            System.out.println("No music is playing to pause.");
+        }
     }
 
     @Override
     public void selectMusic(String music) {
-
+        currentMusic = music;
+        System.out.println("Selected music: " + music);
     }
 
     //Phone Device methods
     @Override
     public void call(String phoneNumber) {
-
+        if(!isCalling){
+            System.out.println("Calling " + phoneNumber);
+            isCalling = true;
+        } else{
+            System.out.println("Already on call.");
+        }
     }
 
     @Override
     public void answerCall() {
-
+        if(isCalling){
+            System.out.println("Call answered.");
+            isCalling = false;
+        } else{
+            System.out.println("No incoming call to answer.");
+        }
     }
 
     @Override
     public void startVoiceMail() {
-
+        if(!isVoiceMail){
+            System.out.println("Voice mail started.");
+            isVoiceMail = true;
+        } else{
+            System.out.println("Voice mail is already active.");
+        }
     }
 
     //Internet Browser methods
     @Override
-    public void addNewTab() {
+    public void displayPage(String url) {
+        currentUrl = url;
+        System.out.println("Displaying page: " + url);
     }
 
     @Override
-    public void displayPage(String url) {
-
+    public void addNewTab() {
+        numTabs++;
+        System.out.println("Added a new tab. Total tabs: " + numTabs);
     }
 
     @Override
     public void updatePage() {
-
+        System.out.println("Page updated: " + currentUrl);
     }
 }
